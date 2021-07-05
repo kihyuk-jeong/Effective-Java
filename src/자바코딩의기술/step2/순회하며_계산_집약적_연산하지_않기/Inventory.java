82507e7d94379e8bc0f26e9aae6e1f81c3ce1671
jungkh405@naver.com <jungkh405@naver.com>
2021-07-05 22:56:12 +0900

    순회하며 계산 집약적 연산하지 않기 p.67

---
+++ src/자바코딩의기술/step2/순회하며_계산_집약적_연산하지_않기/Inventory.java
@@ -0,0 +1,36 @@
+package 자바코딩의기술.step2.순회하며_계산_집약적_연산하지_않기;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Pattern;
+import 자바코딩의기술.step2.순회하며컬렉션수정하지않기.Supply;
+
+class Inventory {
+    private List<Supply> supplies = new ArrayList<>();
+
+    List<Supply> find(String regex) {
+        List<Supply> result = new LinkedList<>();
+        for (Supply supply : supplies) {
+            if(Pattern.matches(regex, supply.toString())) {
+                result.add(supply);
+            }
+        }
+        return result;
+    }
+}
+
+class Inventory_improving {
+    private List<Supply> supplies = new ArrayList<>();
+
+    List<Supply> find(String regex) {
+        List<Supply> result = new LinkedList<>();
+        Pattern pattern = Pattern.compile(regex);
+        for(Supply supply : supplies) {
+            if(pattern.matcher(supply.toString().matches())) {
+                result.add(supply);
+            }
+        }
+        return result;
+    }
+}
